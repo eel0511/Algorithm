cc06313f95a782a26304581e7882503bfb8ad902
SuhyeongLee <eel0511@gmail.com>
2021-08-02 10:11:30 +0900

    Update sw1240.java

--- Sw_Expert_Academy/sw1240.java
+++ Sw_Expert_Academy/sw1240.java
@@ -16,6 +16,9 @@ public class sw1240 {
 			for (int i = 0; i < N; i++) {
 				st = sc.next();
 				if (st.contains("1")) {
+					for (int j = i + 1; j < N; j++) {
+						sc.next();
+					}
 					break;
 				}
 
@@ -23,28 +26,59 @@ public class sw1240 {
 			int start = 0;
 			for (int i = 0; i < st.length(); i++) {
 				if (st.charAt(i) == '1') {
-					start = i;
+					start = i - 3;
 					break;
 
 				}
 			}
 
+			String[] number = { "0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011",
+					"0110111", "0001011" };
+			boolean check = true;
+			for (int i = start; i < start + 4; i++) {
+				if (check) {
+
+					String temp = st.substring(i, i + 7);
+					for (int j = 0; j < number.length; j++) {
+						if (temp.equals(number[j])) {
+
+							if (st.charAt(i + 7) != '1') {
+								check = false;
+								start = i;
+								break;
+							}
+
+						}
+					}
+
+				}
+
+			}
 			int sum = 0;
 			int[] save = new int[8];
 			int index = 0;
 			for (int i = start; i < start + 56; i = i + 7) {
 				String temp = st.substring(i, i + 7);
 				sum = 0;
-				for (int j = 0; j < 7; j++) {
-					sum += temp.charAt(j) - '0';
+				for (int j = 0; j < number.length; j++) {
+					if (temp.equals(number[j])) {
+						sum += j;
+					}
 				}
 				save[index++] = sum;
 
 			}
-			for (int i = 0; i < save.length; i++) {
-				System.out.println(save[i]);
-			}
 
+			int total = (save[0] + save[2] + save[4] + save[6]) * 3 + save[1] + save[3] + save[5] + save[7];
+			if (total % 10 == 0) {
+				total = 0;
+				for (int i = 0; i < save.length; i++) {
+					total += save[i];
+				}
+				System.out.println("#"+test_case+" "+total);
+			} else {
+				System.out.println("#"+test_case+" "+0);
+			}
 		}
 
 	}
