5dafdddd8fb6fbd2bf50f50dedff3561fda4b72a
SuhyeongLee <eel0511@gmail.com>
2021-08-03 22:29:02 +0900

    0803

--- Kotlin/pg순위검색.kt
+++ Kotlin/pg순위검색.kt
@@ -1,6 +1,7 @@
 import java.util.*
 
 class Solution {
+    //맵에 다 넣어서 기본 세팅 맵 저장
     fun setMap(map: HashMap<String, ArrayList<Int>>) {
         val first = arrayOf("cpp", "java", "python", "-")
         val second = arrayOf("backend", "frontend", "-")
@@ -18,6 +19,7 @@ class Solution {
         }
     }
 
+    //값이 들어오면 16가지 경우에 수에 맞게 점수가 모두 입력됨
     fun input(map: HashMap<String, ArrayList<Int>>, info: String, value: Int) {
         val st = info.split(" ")
         val first = arrayOf(st[0], "-")
@@ -44,23 +46,29 @@ class Solution {
             val value = info[i].split(" ")
             input(map, info[i], value[4].toInt())
         }
+
+        //속도를위해 sort후 이진탐색
         for ((_, value) in map) {
             value.sort()
         }
+        map.forEach { print(it) }
         for (i in query.indices) {
             val st = query[i].split(" ")
             val value = st[7].toInt()
             val key = st[0] + st[2] + st[4] + st[6]
             val result = map[key]!!
 
-
+            //JAVA의 binarySearch를 사용하였으며 이것은 값이 있으면 그 값의 인덱스를 리턴
             var num = Collections.binarySearch(result, value)
+
+            //오름차순 정렬이므로  1 2 3 3 4 5 6 7 에서 3 이상을 찾는다면 가장앞의 3의 인덱스를 찾고 길이에서 빼주면 결과가 나옴
             if (num >= 0) {
                 for (j in num - 1 downTo 0) {
                     num = if (result[num] > result[j]) break else j
                 }
                 answer[i] = result.size - num
             } else {
+                //없으면 자기 자리를 찾아 마이너스를 붙여 리턴해줌 10개중에 5번째 자리 뒤에 자기자리면 -6 리턴하는 식
                 answer[i] = result.size + num + 1
             }
         }
