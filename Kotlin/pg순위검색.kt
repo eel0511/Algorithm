210fa61f23444014df9e3626ed1da54c804135f5
SuhyeongLee <eel0511@gmail.com>
2021-08-03 21:08:13 +0900

    Update pg순위검색.kt

--- Kotlin/pg순위검색.kt
+++ Kotlin/pg순위검색.kt
@@ -1,62 +1,57 @@
 import java.util.*
 
 class Solution {
-    fun setMap(map: HashMap<String?, ArrayList<Int>>) {
-        val language = arrayOf("cpp", "java", "python", "-")
-        val position = arrayOf("backend", "frontend", "-")
-        val career = arrayOf("junior", "senior", "-")
-        val soulfood = arrayOf("chicken", "pizza", "-")
-        for (i in language.indices) {
-            for (j in position.indices) {
-                for (k in career.indices) {
-                    for (l in soulfood.indices) {
-                        map[language[i] + position[j] + career[k] + soulfood[l]] = ArrayList()
+    fun setMap(map: HashMap<String, ArrayList<Int>>) {
+        val first = arrayOf("cpp", "java", "python", "-")
+        val second = arrayOf("backend", "frontend", "-")
+        val third = arrayOf("junior", "senior", "-")
+        val fourth = arrayOf("chicken", "pizza", "-")
+
+        for (i in first.indices) {
+            for (j in second.indices) {
+                for (k in third.indices) {
+                    for (l in fourth.indices) {
+                        map[first[i] + second[j] + third[k] + fourth[l]] = ArrayList()
                     }
                 }
             }
         }
     }
 
-    fun inputVal(map: HashMap<String?, ArrayList<Int>>, info: String, value: Int) {
+    fun input(map: HashMap<String, ArrayList<Int>>, info: String, value: Int) {
         val st = info.split(" ")
-        map[st[0] + st[1] + st[2] + st[3]]!!.add(value)
-        map["-" + st[1] + st[2] + st[3]]!!.add(value)
-        map[st[0] + "-" + st[2] + st[3]]!!.add(value)
-        map[st[0] + st[1] + "-" + st[3]]!!.add(value)
-        map[st[0] + st[1] + st[2] + "-"]!!.add(value)
+        val first = arrayOf(st[0], "-")
+        val second = arrayOf(st[1], "-")
+        val third = arrayOf(st[2], "-")
+        val fourth = arrayOf(st[3], "-")
 
-        map["-" + "-" + st[2] + st[3]]!!.add(value)
-        map["-" + st[1] + "-" + st[3]]!!.add(value)
-        map["-" + st[1] + st[2] + "-"]!!.add(value)
-        map[st[0] + "-" + "-" + st[3]]!!.add(value)
-        map[st[0] + "-" + st[2] + "-"]!!.add(value)
-        map[st[0] + st[1] + "-" + "-"]!!.add(value)
-
-        map["-" + "-" + "-" + st[3]]!!.add(value)
-        map["-" + "-" + st[2] + "-"]!!.add(value)
-        map["-" + st[1] + "-" + "-"]!!.add(value)
-        map[st[0] + "-" + "-" + "-"]!!.add(value)
-
-        map["-" + "-" + "-" + "-"]!!.add(value)
-
+        for (i in first.indices) {
+            for (j in second.indices) {
+                for (k in third.indices) {
+                    for (l in fourth.indices) {
+                        map[first[i] + second[j] + third[k] + fourth[l]]!!.add(value)
+                    }
+                }
+            }
+        }
     }
 
     fun solution(info: Array<String>, query: Array<String>): IntArray {
         val answer = IntArray(query.size)
-        val infoMap = HashMap<String?, ArrayList<Int>>()
-        setMap(infoMap)
+        val map = HashMap<String, ArrayList<Int>>()
+        setMap(map)
         for (i in info.indices) {
             val value = info[i].split(" ")
-            inputVal(infoMap, info[i], value[4].toInt())
+            input(map, info[i], value[4].toInt())
         }
-        for ((_, value) in infoMap) {
+        for ((_, value) in map) {
             value.sort()
         }
         for (i in query.indices) {
             val st = query[i].split(" ")
             val value = st[7].toInt()
             val key = st[0] + st[2] + st[4] + st[6]
-            val result = infoMap[key]!!
+            val result = map[key]!!
 
 
             var num = Collections.binarySearch(result, value)
