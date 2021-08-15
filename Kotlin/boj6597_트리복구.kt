41e5ae9caf86f3833cbd98c6b918fed2a56839be
SuhyeongLee <eel0511@gmail.com>
2021-08-15 17:20:23 +0900

    Create boj6597_트리복구.kt

---
+++ Kotlin/boj6597_트리복구.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+var preorder = ""
+var inorder = ""
+fun main() = with(Scanner(System.`in`)) {
+    var temp = readLine()
+    while (!temp.isNullOrEmpty()) {
+
+        preorder = temp.split(" ")[0]
+        inorder = temp.split(" ")[1]
+        postorder(0, preorder.length, 0)
+        println()
+        temp = readLine()
+    }
+
+}
+
+fun postorder(start: Int, end: Int, root: Int) {
+    for (i in start until end) {
+        //전위순회의 root값을 inorder에서 찾으면 inorder에서 root-1은 왼쪽자식, root+1은 오른쪽자식
+        //for loop을 다돌아서 마지막 노드에 왔을때 출력
+        if (preorder[root] == inorder[i]) {
+            postorder(start, i, root + 1)
+            postorder(i + 1, end, root + 1 + i - start)
+            print("${preorder[root]}")
+        }
+    }
+}
