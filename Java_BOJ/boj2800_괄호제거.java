8d36c1166893fc690aaf8938de981e07b7260437
SuhyeongLee <eel0511@gmail.com>
2021-08-17 21:03:02 +0900

    Update boj2800_괄호제거.java

--- Java_BOJ/boj2800_괄호제거.java
+++ Java_BOJ/boj2800_괄호제거.java
@@ -15,7 +15,7 @@ public class boj2800_괄호제거 {
 		String first = sc.nextLine();
 		st = first.split("");
 
-		//String 배열로 입력받은 식에서 괄호만 따로 쌍을 잡아서 map에 넣어줌
+		// String 배열로 입력받은 식에서 괄호만 따로 쌍을 잡아서 map에 넣어줌
 		for (int i = 0; i < st.length; i++) {
 			if (st[i].equals("(")) {
 				stack.push(i);
@@ -24,14 +24,14 @@ public class boj2800_괄호제거 {
 				map.put(temp, i);
 			}
 		}
-		//백트래킹
+		// 백트래킹
 		dfs(0, "");
-		
-		//정렬을 위해 set을 List에 담고 정렬 후 출력
+
+		// 정렬을 위해 set을 List에 담고 정렬 후 출력
 		List<String> list = new ArrayList<>(set);
 		Collections.sort(list);
 
-		//자기자신 제외
+		// 자기자신 제외
 		for (String str : list) {
 			if (!str.equals(first)) {
 				System.out.println(str);
@@ -41,28 +41,27 @@ public class boj2800_괄호제거 {
 
 	public static void dfs(int index, String s) {
 
-		//종료조건
-		if (index >= st.length) {
-			//중복방지를 위해 set사용
+		// 종료조건
+		if (index == st.length) {
+			// 중복방지를 위해 set사용
 			set.add(s);
 			return;
-		} else {
-			//map에 들어있는 index순서가 오면 괄호 이므로
-			if (map.containsKey(index)) {
-
-				//괄호를 지우고 dfs
-				st[index] = "";
-				st[map.get(index)] = "";
-				dfs(index + 1, s + st[index]);
-
-				//괄호를 풀어주고 dfs
-				st[index] = "(";
-				st[map.get(index)] = ")";
-
-			}
-			//지속해서 한칸씩 이동하면서 s에 식을 한칸씩담아나감
-			dfs(index + 1, s + st[index]);
 		}
+		// map에 들어있는 index순서가 오면 괄호 이므로
+		if (map.containsKey(index)) {
+
+			// 괄호를 지우고 dfs
+			st[index] = "";
+			st[map.get(index)] = "";
+			dfs(index + 1, s + st[index]);
+
+			// 괄호를 풀어주고 dfs
+			st[index] = "(";
+			st[map.get(index)] = ")";
+
+		}
+		// 지속해서 한칸씩 이동하면서 s에 식을 한칸씩담아나감
+		dfs(index + 1, s + st[index]);
 
 	}
 
